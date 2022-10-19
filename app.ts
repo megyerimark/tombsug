afd46cb58642977d4087dd68d119d45df97c6d28
Márk Megyeri <megyko1234@gmail.com>
2022-10-19 15:54:46 +0200

    App.ts elkezdése

---
+++ app.ts
@@ -0,0 +1,9 @@
+//  File: app.ts
+// Author: Megyeri Márk Máté
+// Copyright: 2022, Megyeri Márk Máté
+// Group: Szoft II/N
+// Date: 2022-10-19
+// Github: https://github.com/megyerimark/
+//
+
+const oldal=document.querySelector(#oldal) as HTMLInputElement;
